2a7f41977dc7f7e7e37c2b6a42e2c7032a90ab83
Liu Renhang <r65liu@uwaterloo.ca>
2022-10-05 15:58:04 -0400

    fix: sender_id

--- src/test_queue.c
+++
@@ -1,9 +0,0 @@
-//
-// Created by 28379 on 10/5/2022.
-//
-
-#include "../src/queue.h"
-
-int main() {
-
-}
--- test/print.h
+++ test/print.h
@@ -4,6 +4,8 @@
 
 #ifndef K2_PRINT_H
 #define K2_PRINT_H
+
+#include "../src/message_passing.h"
 void printf_( const char* format, ... );
 void print_task(Task* t);
 void print_send(SendArgs* send);
