1f5b22c03930b0750ecdd788016af56d2eeb2684
h44ding <h44ding@nettop54.student.cs.uwaterloo.ca>
2022-10-06 00:50:46 -0400

    add time measurements

--- src/test_user_tasks.c
+++ src/test_user_tasks.c
@@ -3,6 +3,7 @@
 //
 #include "syscall.h"
 #include "printf.h"
+#include "timer.h"
 
 void task_a() {
   printf("Tid of A: %d\r\n", MyTid());
@@ -19,19 +20,28 @@ void task_b() {
   printf("Tid of B: %d\r\n", MyTid());
   char msg[] = "Hello!";
   char reply[10];
+  u32 time_a = timer_get_system_time_count();
   int len_reply = Send(1, msg, sizeof msg, reply, 10);
+  u32 time_b = timer_get_system_time_count();
+  u32 delta_time = time_b - time_a;
   printf("got reply %s with length:%d\r\n", reply, len_reply);
+
+  time_a = timer_get_system_time_count();
+  time_b = timer_get_system_time_count();
+  delta_time = delta_time - (time_b - time_a);
+
+  printf("Send operation took %dms\r\n", timer_count_to_ms(delta_time));
 }
 
-//void initial_user_task() {
-//  printf("start initial user task\r\n");
-//  i32 tid;
-//
-//  tid = Create(9, task_a);
-//  printf("A created\r\n");
-//
-//  tid = Create(8, task_b);
-//  printf("B created\r\n");
-//
-//  Exit();
-//}
+void initial_user_task() {
+ printf("start initial user task\r\n");
+ i32 tid;
+
+ tid = Create(9, task_a);
+ printf("A created\r\n");
+
+ tid = Create(8, task_b);
+ printf("B created\r\n");
+
+ Exit();
+}
